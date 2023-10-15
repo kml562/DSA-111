f3fe73d16e50182c428959a3c8414b1c4e5c871e
kml562 <111355794+kml562@users.noreply.github.com>
2023-10-15 11:33:49 +0530

    update

---
+++ week1/day3/day3.js
@@ -0,0 +1,21 @@
+// n=5;
+// A 
+// A B A
+// A B C B A
+// A B C D C B A 
+// A B C D E D C B A
+
+function pyramid(n){
+    let alpha= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let str="";
+    for(let r=1;r<=n;r++){
+ let nstr="";
+ for(let j=1;j<r+(r-1);j++){
+if(j<=r){
+nstr+= alpha[j-1]
+}else{
+    
+}
+ }
+    }
+}
