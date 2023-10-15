2fc11cbf31682c1f3aa0159b229ac903f1d2a691
kml562 <111355794+kml562@users.noreply.github.com>
2023-10-15 11:33:10 +0530

    done

---
+++ week1/day5/prb1.js
@@ -0,0 +1,36 @@
+// let s = ",A man, a plan, a canal: Panama"
+// s= s.toUpperCase();
+// for(let i=0;i<s.length;i++){
+//     if(s[i].toUpperCase()){
+//         console.log(s[i],"true")
+//     }else{
+//         console.log(s[i], 'false')
+//     }
+// }
+  
+// //     s= s.toUpperCase()
+// //    console.log(s[1].toUpperCase(),"lsd")
+// //    if(s[0].toUpperCase()===true){
+// //   console.log('yues')
+// //    }else{
+// //     console.log("first")
+// //    }
+// //     console.log(s)
+// //    let i=0; let j= s.length-1;
+// //    while(i<j){
+// //        while(!s[i].toUpperCase()){
+// //            console.log(s[i])
+// //          i++;
+// //        };
+// //        while(!s[j].toUpperCase()){ j--;
+// //        console.log(s[j])}
+// //      if(s[i]!==s[j]){ return false; }
+// //      i++; j--
+// //    };
+// //    return true
+
+// //   isPalindrome("A man, a plan, a canal: Panama")
+let char = "0PO";
+let ascii = char.charCodeAt(0)
+let ascii2 = char.charCodeAt(2)
+console.log(ascii,ascii2)
